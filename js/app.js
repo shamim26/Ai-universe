eb126faa995bd600ed0c0a871db18e08d423f605
shamim26 <shamimreja101@gmail.com>
2023-03-02 21:51:17 +0600

    html and css design completed

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const loadData= () =>{
+    fetch('https://openapi.programming-hero.com/api/ai/tools')
+    .then(res => res.json())
+    .then(data => console.log(data.data.tools))
+}
+
+const displayData = arrOfData => 
+
+loadData();
